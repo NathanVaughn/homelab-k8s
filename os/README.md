9a2df2cfafa0defe74bd3e63c5164531cf0cbc5b
Nathan Vaughn <nvaughn51@gmail.com>
2024-07-05 21:13:44 +0000

    Make autoinstall prep

--- os/README.md
+++ os/README.md
@@ -1,7 +1,8 @@
 1. Download desired Ubuntu image
 2. Create bootable flash drive
-3. Add `autoinstall.yaml` to flash drive root
+3. Run `prep.py`
+4. Add `autoinstall.yaml` to flash drive root
 
 References:
 
-- [https://canonical-subiquity.readthedocs-hosted.com/en/latest/tutorial/providing-autoinstall.html#providing-autoinstall](https://canonical-subiquity.readthedocs-hosted.com/en/latest/tutorial/providing-autoinstall.html#providing-autoinstall)
+- <https://canonical-subiquity.readthedocs-hosted.com/en/latest/tutorial/providing-autoinstall.html#providing-autoinstall>
---
+++ os/prep.py
@@ -0,0 +1,34 @@
+import yaml
+import os
+import argparse
+import urllib.request
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_FILE = os.path.join(THIS_DIR, "autoinstall.tmpl.yaml")
+OUTPUT_FILE = os.path.join(THIS_DIR, "autoinstall.yaml")
+
+
+def main(hostname: str) -> None:
+    with open(TEMPLATE_FILE, "r") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+    # Download the SSH public key
+    ssh_key_url = (
+        "https://raw.githubusercontent.com/NathanVaughn/public-keys/main/ssh.pub"
+    )
+    with urllib.request.urlopen(ssh_key_url) as response:
+        ssh_key = response.read().decode("utf-8").strip()
+
+    data["autoinstall"]["identity"]["hostname"] = hostname
+    data["autoinstall"]["user-data"]["users"][0]["ssh_authorized_keys"] = [ssh_key]
+
+    with open(OUTPUT_FILE, "w") as f:
+        yaml.dump(data, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostname", type=str, help="The hostname of the machine.")
+    args = parser.parse_args()
+
+    main(args.hostname)
