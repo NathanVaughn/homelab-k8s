a2538231207f8f8da9c7a10670f2e8af0720eac9
Nathan Vaughn <nath@nvaughn.email>
2024-11-15 19:52:43 -0600

    Update prep

--- os/prep.py
+++ os/prep.py
@@ -2,13 +2,14 @@ import yaml
 import os
 import argparse
 import urllib.request
+import subprocess
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_FILE = os.path.join(THIS_DIR, "autoinstall.tmpl.yaml")
 OUTPUT_FILE = os.path.join(THIS_DIR, "autoinstall.yaml")
 
 
-def main(hostname: str) -> None:
+def main(hostname: str, password: str) -> None:
     with open(TEMPLATE_FILE, "r") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -20,7 +21,11 @@ def main(hostname: str) -> None:
         ssh_key = response.read().decode("utf-8").strip()
 
     data["autoinstall"]["identity"]["hostname"] = hostname
-    data["autoinstall"]["user-data"]["users"][0]["ssh_authorized_keys"] = [ssh_key]
+    data["autoinstall"]["identity"]["password"] = subprocess.check_output(
+        ["mkpasswd", "-m", "sha-512", password]
+    ).decode("utf-8").strip()
+
+    data["autoinstall"]["ssh"]["authorized-keys"] = [ssh_key]
 
     with open(OUTPUT_FILE, "w") as f:
         yaml.dump(data, f)
@@ -29,6 +34,7 @@ def main(hostname: str) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("hostname", type=str, help="The hostname of the machine.")
+    parser.add_argument("password", type=str, help="The password of the machine.")
     args = parser.parse_args()
 
-    main(args.hostname)
+    main(args.hostname, args.password)
