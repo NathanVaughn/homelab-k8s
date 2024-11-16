f9de064846e2c4c60857c4f1d19ee86d26370bc6
Nathan Vaughn <nath@nvaughn.email>
2024-11-16 02:09:38 +0000

    Simplify

--- software/configure.py
+++ software/configure.py
@@ -1,52 +1,7 @@
-from pyinfra.operations import files, server, apt
+from pyinfra.operations import apt, files, server
 
 temp_k3s_file = "/tmp/install_k3s.sh"
-smartd_dir = "/var/log/smartd"
-smartd_config = "/etc/default/smartmontools"
-smartd_logrotate = "/etc/logrotate.d/smartd"
 resolvconf_config = "/etc/resolvconf/resolv.conf.d/head"
-
-# region Install lm-sensors
-apt.packages(
-    name="Install lm-sensors",
-    packages=["lm-sensors"],
-    update=True,
-    _sudo=True,
-)
-# endregion
-
-# region Install smartmontools
-# https://learn.netdata.cloud/docs/collecting-metrics/hardware-devices-and-sensors/s.m.a.r.t.#configure-smartd-to-write-attribute-information-to-files
-apt.packages(
-    name="Install smartmontools",
-    packages=["smartmontools"],
-    update=True,
-    _sudo=True,
-)
-
-files.block(
-    name="Edit smartd configuration file",
-    path=smartd_config,
-    content=f'smartd_opts="-A {smartd_dir} -i 600',
-)
-
-files.directory(name="Create smartd log directory", path=smartd_dir, present=True)
-
-apt.packages(
-    name="Install logrotate",
-    packages=["logrotate"],
-    update=True,
-    _sudo=True,
-)
-
-files.block(
-    name="Edit smartd logrotate configuration",
-    path=smartd_logrotate,
-    content="/var/log/smartd/* {\n\tweekly\n\tmaxsize 10M\n}",
-)
-
-server.service(name="Start smartd", service="smatd", running=True, enabled=True)
-# endregion
 
 # region Install k3s
 files.download(
