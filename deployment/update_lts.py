dfbe29aa5843bc33eb1d79182bfee1d9732ac0fa
Nathan Vaughn <nath@nvaughn.email>
2026-06-05 21:47:51 +0000

    Add lts upgrade script

---
+++ deployment/update_lts.py
@@ -0,0 +1,23 @@
+from pyinfra.operations import apt, python, server
+
+
+def upgrade_lts():
+    apt.update(name="Update apt cache", _sudo=True)
+    apt.packages(
+        name="Install update-manager-core",
+        packages=["update-manager-core"],
+        _sudo=True,
+    )
+    apt.upgrade(name="apt upgrade --autoremove", auto_remove=True, _sudo=True)
+    server.shell(
+        name="Upgrade the node",
+        commands="do-release-upgrade -d -f DistUpgradeViewNonInteractive",
+        _sudo=True,
+    )
+
+
+python.call(
+    name="Upgrade Ubuntu LTS",
+    function=upgrade_lts,
+    _serial=True,
+)
