8bd5148ee1f9ab21cecd7eb77142f350aecbea5d
Nathan Vaughn <nath@nvaughn.email>
2026-09-02 22:27:20 +0000

    Enable direct serve and metadata caching

--- cluster/git-pkgs/README.md
+++ cluster/git-pkgs/README.md
@@ -9,6 +9,8 @@ export AWS_REGION=$AWS_REGION
 export POSTGRES_PASSWORD=$DB_PASSWORD
 export S3_BUCKET=$S3_BUCKET
 export S3_ENDPOINT=$S3_ENDPOINT
+export PROXY_STORAGE_DIRECT_SERVE=true
+export PROXY_STORAGE_DIRECT_SERVE_BASE_URL=$PROXY_STORAGE_DIRECT_SERVE_BASE_URL
 # change dollar sign variables above this line
 kubectl apply -f namespace.yaml
 
@@ -19,6 +21,8 @@ kubectl -n git-pkgs create secret generic git-pkgs-env \
 --from-literal=PROXY_DATABASE_URL=postgres://gitpkgs:$POSTGRES_PASSWORD@git-pkgs-postgresql-service.git-pkgs.svc.cluster.local:5432/gitpkgs?sslmode=disable \
 --from-literal="PROXY_STORAGE_URL=s3://$S3_BUCKET?endpoint=$S3_ENDPOINT&prefix=pkgs/&s3ForcePathStyle=true" \
 --from-literal=POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+--from-literal=PROXY_STORAGE_DIRECT_SERVE=$PROXY_STORAGE_DIRECT_SERVE \
+--from-literal=PROXY_STORAGE_DIRECT_SERVE_BASE_URL=$PROXY_STORAGE_DIRECT_SERVE_BASE_URL \
 --dry-run=client -o yaml > secret.yaml
 
 kubeseal --format=yaml --cert=../sealed-secrets/sealed-secrets-public-key.pem < secret.yaml > sealed-secret.yaml
---
+++ cluster/git-pkgs/create-config.py
@@ -0,0 +1,67 @@
+import pathlib
+
+import yaml
+import yaml.representer
+
+THIS_DIR = pathlib.Path(__file__).parent
+CLUSTER_DIR = THIS_DIR.parent
+
+# Run this script to generate the config map
+
+
+def str_presenter(
+    dumper: yaml.Dumper | yaml.representer.SafeRepresenter, data: str
+) -> yaml.Node:
+    """configures yaml for dumping multiline strings"""
+    # https://github.com/yaml/pyyaml/issues/240#issuecomment-1096224358
+    if data.count("\n") > 0:  # check for multiline string
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
+
+yaml.add_representer(str, str_presenter)
+# to use with safe_dum
+yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
+
+
+def main() -> None:
+    repos = {}
+
+    for yaml_file in CLUSTER_DIR.glob("**/*.y*ml"):
+        with open(yaml_file) as fp:
+            docs = yaml.safe_load_all(fp)
+            for doc in docs:
+                if not (
+                    doc.get("apiVersion") == "source.toolkit.fluxcd.io/v1"
+                    and doc.get("kind") == "HelmRepository"
+                ):
+                    continue
+
+                print(f"Parsing {yaml_file}")
+
+                repos[doc["metadata"].get("namespace")] = doc["spec"].get("url")
+
+    # sort the repos
+    repos = dict(sorted(repos.items()))
+
+    # create inner data object
+    inner_data = {"upstream": {"helm": repos}}
+
+    # convert to text
+    inner_data_text = yaml.dump(inner_data, indent=2)
+
+    # wrap with outer data
+    outer_data = {
+        "apiVersion": "v1",
+        "kind": "ConfigMap",
+        "metadata": {"name": "git-pkgs-configmap", "namespace": "git-pkgs"},
+        "data": {"config.yaml": inner_data_text},
+    }
+
+    # write to file
+    with open(THIS_DIR.joinpath("configmap.yaml"), "w") as fp:
+        yaml.dump(outer_data, fp)
+
+
+if __name__ == "__main__":
+    main()
